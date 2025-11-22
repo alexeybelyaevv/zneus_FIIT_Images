dd97fc0bdac12f7cee7db37b84419a1bb9a4479d
AdamGlog <adamglog9919@gmail.com>
2025-11-22 22:41:53 +0100

    added pytorch, and other modules, loading the dataset by ImageFolder and DataLoader, splitting the dataset, and resizing every image to 224x224 for easier approach. Updated translate_names.py to translate only to english

--- translate_names.py
+++ translate_names.py
@@ -1,12 +1,16 @@
 #translates the directory names in the images archive
-#move this file to archive where translate file is!!!
-import translate
+translate = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat", "mucca": "cow", "pecora": "sheep", "scoiattolo": "squirrel", "ragno": "spider"}
 import os
-basedir = 'C:/<your_path_to_archive>/archive/raw-img'
+working_dir = os.getcwd()
+print(working_dir)
+basedir = os.path.join(working_dir, "archive/raw-img")
+#or if the basedir doesnt work use this >
+#basedir = "<path_to_archive>/archive/raw-img"
+print(basedir)
 for fn in os.listdir(basedir):
     if not os.path.isdir(os.path.join(basedir, fn)):
         continue # Not a directory
-    if fn not in translate.translate: 
+    if fn not in translate: 
         continue
-    print(translate.translate[fn])
-    os.rename(os.path.join(basedir,fn), os.path.join(basedir, translate.translate[fn]))
+    print(translate[fn])
+    os.rename(os.path.join(basedir,fn), os.path.join(basedir, translate[fn]))
